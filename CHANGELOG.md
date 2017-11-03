5add664a6c7fbac9982a402bfcf095aa9b48ea41
Thomas Körner <t.koerner@heimrich-hannot.de>
2017-11-03 11:53:25 +0100

    no folder name sanitization, added changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [1.0.2] - 2017-11-03
+
+### Fixed
+* sanitized also folder names
+
+## [1.0.1] - 2017-05-17
+
+### Added
+* missing composer dependencies
+
+## [1.0.0] - 2016-11-22
+
+* initial version
--- dca/tl_files.php
+++ dca/tl_files.php
@@ -7,10 +7,20 @@ $arrDca = &$GLOBALS['TL_DCA']['tl_files'];
  */
 $arrDca['fields']['name']['save_callback'][] = array('tl_files_upload_filename_sanitizer', 'sanitizeFilename');
 
+
 class tl_files_upload_filename_sanitizer
 {
-    public function sanitizeFilename($varValue)
+    /**
+     * @param string $varValue
+     * @param DataContainer $dc
+     * @return string
+     */
+    public function sanitizeFilename($varValue, $dc)
     {
+        if ($dc->activeRecord->type == 'folder')
+        {
+            return $varValue;
+        }
         return \HeimrichHannot\Haste\Util\Files::sanitizeFileName($varValue);
     }
 }
